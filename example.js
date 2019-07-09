fb7486e059584d7289723838655590cf74279f7b
Rachel <rachelbaee23@gmail.com>
2019-07-09 14:58:38 -0700

    Initial Commit

---
+++ example.js
@@ -0,0 +1,11 @@
+alert("Welcome To Sneaker News!")
+
+function bigImg(x){
+  x.style.height = "600px";
+  x.style.width = "600px";
+}
+
+function originalImg(x){
+  x.style.height = "400px";
+  x.style.width = "400px";
+}
